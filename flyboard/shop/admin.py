e4741d2d2f3eacd2cc3be809b365245d87d96465
123 <123@mail.ru>
2022-09-08 22:17:33 +0500

    Добавлена модель, сделаны миграции

--- flyboard/shop/admin.py
+++ flyboard/shop/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Products
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'is_published')
+    list_display_links = ('name', 'id')
+    search_fields = ('name',  'price','id')
+
+
+admin.site.register(Products, ProductsAdmin)
+
--- flyboard/shop/models.py
+++ flyboard/shop/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Products(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
+    description = models.TextField(blank=True, verbose_name='Описание')
+    price = models.IntegerField(verbose_name='Цена', blank=True)
+    guarantee = models.IntegerField(verbose_name='Гарантия', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ['name']
+
