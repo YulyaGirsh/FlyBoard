b363c5d27620ffdc3a5206ecf6fe8a3ff90efc67
123 <123@mail.ru>
2022-09-09 19:50:46 +0500

    Настроена админка

--- flyboard/shop/admin.py
+++ flyboard/shop/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Products
+from .models import Products, Category
 
 
 class ProductsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'is_published')
+    list_display = ('id', 'name', 'price', 'is_published', 'category')
     list_display_links = ('name', 'id')
-    search_fields = ('name',  'price','id')
+    search_fields = ('name', 'price', 'id')
+    list_editable = ('is_published', 'category')
+    list_filter = ('is_published', 'category')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
 
 
 admin.site.register(Products, ProductsAdmin)
-
+admin.site.register(Category, CategoryAdmin)
--- flyboard/shop/models.py
+++ flyboard/shop/models.py
@@ -9,9 +9,25 @@ class Products(models.Model):
     price = models.IntegerField(verbose_name='Цена', blank=True)
     guarantee = models.IntegerField(verbose_name='Гарантия', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
         ordering = ['name']
 
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Название категории')
+
+    def __str__(self):
+        return self.title
+
+
+class Meta:
+    verbose_name = 'Категория'
+    verbose_name_plural = 'Категории'
+    ordering = ['title']
