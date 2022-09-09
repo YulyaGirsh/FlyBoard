aed456f1688514c51eca0aa03cc4454fdc25fa22
123 <123@mail.ru>
2022-09-09 19:25:09 +0500

    Добавлены товары

--- flyboard/shop/views.py
+++ flyboard/shop/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Products
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html', {'Title': 'Главная'})
+
+    # res = '<h1>Список новостей </h1>'
+    # for item in goods:
+    #     res+=f'<div> {item.name}<div/>\n'
+    # return HttpResponse(res)
+    return render(request, 'shop/index.html', {'title': 'Главная', })
 
 
 def contact(request):
-    return render(request, 'shop/contact.html',{'Title': 'Контакты'})
+    return render(request, 'shop/contact.html', {'title': 'Контакты'})
 
 
 def shop(request):
-    return render(request, 'shop/shop.html',{'Title': 'Каталог'})
+    goods = Products.objects.all()
+    return render(request, 'shop/shop.html', {'title': 'Каталог', 'goods': goods})
